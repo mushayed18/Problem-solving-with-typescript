d925ad6df6a8ab814a55c7295b2cbf69d9bd68eb
mushayed125 <kmr18adi@gmail.com>
2025-11-18 00:34:51 +0600

    Problem 6 has been solved

--- solution.ts
+++ solution.ts
@@ -55,4 +55,21 @@ function filterActiveUsers(users: User[]): User[] {
 }
 
 
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const availability = book.isAvailable ? "Yes" : "No";
+  console.log(
+    `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
+  );
+}
+
+
+
+
 
