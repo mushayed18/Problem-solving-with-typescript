e21a8616d5a5578b343b7fa570998f11f710a3e0
mushayed125 <kmr18adi@gmail.com>
2025-11-17 20:19:17 +0600

    Problem 3 has been solved

--- solution.ts
+++ solution.ts
@@ -20,3 +20,18 @@ function getLength(value: string | unknown[]): number {
   return 0;
 }
 
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
